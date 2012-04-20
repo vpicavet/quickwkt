48349f95fabed95a82a2b2a20ca2782f63f3a878
Vincent Picavet <vincent.picavet@oslandia.com>
2012-04-20 14:25:58 +0200

    commit initial du projet quickwkt : ajout fichiers

---
+++ README.rst
@@ -0,0 +1,56 @@
+QuickWKT Qgis Plugin
+====================
+
+This Qgis Plugin makes it possible to show features in QGIS using WKT
+
+This plugin is supposed to be installed via the plugin manager of QGIS
+
+
+Code: https://github.com/elpaso/quickwkt
+
+Plugin: http://plugins.qgis.org/plugins/QuickWKT/
+
+
+Original Author: Alessandro Pasotti
+
+Contributing Autors:
+
+* Richard Duivenvoorde
+* Ryan Lewis
+
+License
+-------
+
+GPLv3
+
+
+Version 1.8
+-----------
+
+- added example combo
+
+Version 1.7
+-----------
+
+- fixed missing icon
+
+Version 1.6
+-----------
+
+- add support for hexEWKB
+- add support for linearring and geometry collections in WKT format
+
+
+Version 1.5
+-----------
+
+- add more then one feature in the txt area, also a set of more geometry types
+- create only layer type if needed: if you only paste a wkt of a point, then only a point layer is created
+- lines which cannot be parsed combine to a warning
+- user can either choose if the layers should be reused, OR on every use of plugin should create new layers
+- addition of README
+
+Version 1.0
+-----------
+
+- Initial version
---
+++ __init__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+Name			 	 : QuickWKT
+Description          : GQuick WKT viewer
+Date                 : 11/Oct/2010
+copyright            : (C) 2010 by ItOpen
+email                : info@itopen.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+def name():
+    return "QuickWKT"
+
+def description():
+    return "Quick WKT/WKB viewer, this Qgis Plugin opens a dialog where the user can paste (E)WKT and WKB code and see it on the map. Pasted data are stored in a temporay (memory) layer and are completely lost when the user quits QGIS."
+
+def version():
+    return "1.8"
+
+def qgisMinimumVersion():
+    return "1.5"
+
+def classFactory(iface):
+    # load GeoCoding class from file GeoCoding
+    from QuickWKT import QuickWKT
+    return QuickWKT(iface)
+
+def experimental():
+    return False
+
+def homepage():
+    return 'http://www.itopen.it/2010/10/21/wkt-on-the-fly-qgis-plugin/'
+
+def repository():
+    return 'https://github.com/elpaso/quickwkt'
+
+def tracker():
+    return 'https://github.com/elpaso/quickwkt/issues'
+
+def icon():
+    return 'quickwkt_icon.png'
+
